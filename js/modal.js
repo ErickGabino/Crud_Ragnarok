9b0eb28c89a1e34fab2a32daef9fda9af60a48f2
Erick Gabino <93292129+ErickGabino@users.noreply.github.com>
2023-10-26 19:02:31 -0600

    add god y remove gods y giants funcionan

--- js/modal.js
+++ js/modal.js
@@ -1,57 +1,31 @@
-// Fetch para obtener los datos de la base de datos a traves de http de la BD
-async function getTableData(){
-	await fetch("http://localhost:3800/api/gods")
-	  .then((response) => response.json())
-	  .then((dataGods) => mostrarDioses(dataGods))
-	  .catch((error) => {
-		console.warn(`Error al obtener la lista de gods`);
-	  });
-	
-	 await fetch("http://localhost:3800/api/giants")
-	  .then((response) => response.json())
-	  .then((dataGiant) => mostrarGigantes(dataGiant))
-	  .catch((error) => {
-		console.warn(`Error al obtener la lista de giants`);
-	  });
-	}
-	
-//invocar funcion asincrona llamar datos tabla
-getTableData();
+// const { response } = require("express");
 
-// Declaracion y escucha de los botones 
-let btnAddGod = document.getElementById('addGod');
-btnAddGod.addEventListener("click", addGod)
+// obtener datos 
+async function obtenerDatos(){
+	// Fetch para obtener los datos de la base de datos a traves de http de la BD
+		await fetch("http://localhost:3800/api/gods")
+		.then((response) => response.json())
+		.then((dataGods) => mostrarDioses(dataGods))
+		.catch((error) => {
+		  console.warn(`Error al obtener la lista de gods`);
+		});
+		
+		await fetch("http://localhost:3800/api/giants")
+		.then((response) => response.json())
+		.then((dataGiant) => mostrarGigantes(dataGiant))
+		.catch((error) => {
+		  console.warn(`Error al obtener la lista de giants`);
+		});
+}
 
-var btnRemoveGod = document.getElementById('removeGod');
-btnRemoveGod.addEventListener("click", removeGod)
-
-var btnEditGod = document.getElementById('editGod');
-btnEditGod.addEventListener("click", editGod);
-
-var btnSeeGod = document.getElementById('seeGod');
-btnSeeGod.addEventListener("click", seeGod)
-
-let btnAddGiant = document.getElementById('addGiant');
-btnAddGiant.addEventListener("click", addGiant)
-
-var btnRemoveGiant = document.getElementById('removeGiant');
-btnRemoveGiant.addEventListener("click", removeGiant)
-
-var btnEditGiant = document.getElementById('editGiant');
-btnEditGiant.addEventListener("click", editGiant)
-
-var btnSeeGiant = document.getElementById('seeGiant');
-btnSeeGiant.addEventListener("click", seeGiant)
+// cargar los documentos al cargar la pantalla 
+document.addEventListener('DOMContentLoaded', obtenerDatos);
 
 // Elementos del modal guardado en variables
 var modal = document.getElementById('modal');
 var modalBody = modal.querySelector('.modal-body');
 var modalTitle = modal.querySelector('.modal-title');
 var modalFooter = modal.querySelector('.modal-footer');	
-
-function clearModal(){
-	modalTitle.innerHTML="";
-}
 
 let tbodyGods = document.getElementById('tbodyGods');
 let tbodyGiants = document.getElementById('tbodyGiants');
@@ -69,9 +43,9 @@ function mostrarDioses(dioses){
 				<td>${dios.God}</td>
 				<td>${dios.Power}</td>
 				<td class="d-flex justify-content-center flex-column"">
-					<button class="btn btn-danger removeGod" class="removeGod" id="removeGod" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-xmark"></i><p class="hidden-md">Remove God</p></span></button>
-					<button class="btn btn-warning editGod" class="editGod" id="editGod" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-pen"></i><p class="hidden-md">Edit God</p></span></button>
-					<button class="btn btn-info seeGod" class="seeGod" id="seeGod" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-eye"></i><p class="hidden-md">See God</p></span></button>
+					<button class="btn btn-danger removeGod" class="removeGod" id="removeGod" type="button"><span><i class="fa-solid fa-xmark"></i><p class="hidden-md">Remove God</p></span></button>
+					<button class="btn btn-warning editGod" class="editGod" id="editGod" type="button"><span><i class="fa-solid fa-pen"></i><p class="hidden-md">Edit God</p></span></button>
+					<button class="btn btn-info seeGod" class="seeGod" id="seeGod" type="button"><span><i class="fa-solid fa-eye"></i><p class="hidden-md">See God</p></span></button>
 				</td>
 			</tr>
 		`;
@@ -89,9 +63,9 @@ function mostrarGigantes(gigantes){
 				<td>${gigante.Giant}</td>
 				<td>${gigante.Power}</td>
 				<td class="d-flex justify-content-center flex-column">
-					<button class="btn btn-danger removeGiant" id="removeGiant" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-xmark"></i><p class="hidden-md">Remove Giant</p></span></button>
-					<button class="btn btn-warning editGiant" id="editGiant" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-pen"></i><p class="hidden-md">Edit Giant</p></span></button>
-					<button class="btn btn-info seeGiant" id="seeGiant" type="button" data-bs-toggle="modal" data-bs-target="#modal"><span><i class="fa-solid fa-eye"></i><p class="hidden-md">See Giant</p></span></button>
+					<button class="btn btn-danger removeGiant" id="removeGiant" type="button"ñ><span><i class="fa-solid fa-xmark"></i><p class="hidden-md">Remove Giant</p></span></button>
+					<button class="btn btn-warning editGiant" id="editGiant" type="button"ñ><span><i class="fa-solid fa-pen"></i><p class="hidden-md">Edit Giant</p></span></button>
+					<button class="btn btn-info seeGiant" id="seeGiant" type="button"ñ><span><i class="fa-solid fa-eye"></i><p class="hidden-md">See Giant</p></span></button>
 				</td>
 			</tr>
 		`;
@@ -101,42 +75,6 @@ function mostrarGigantes(gigantes){
 }
 
 // FUNCIONES PARA MOSTRAR EN EL MODAL 
-function addGod(){
-	clearModal();
-	modalTitle.innerHTML = btnAddGod.textContent;
-	modalBody.innerHTML = `
-	<form>
-		<div class="mb-3">
-			<label for="nameGod" class="form-label">Name in the Netflix Serie</label>
-			<input type="text" class="form-control" id="nameGod" aria-describedby="nameLabel">
-		</div>
-		<div class="mb-3">
-			<label for="nameGodRepresentation" class="form-label">God's name</label>
-			<input type="text" class="form-control" id="nameGodRepresentation" aria-describedby="nameLabel">
-		</div>
-		<div class="mb-3">
-			<label for="powerGod" class="form-label">God's powers</label>
-			<input type="text" class="form-control" id="powerGod" aria-describedby="powerLabel">
-		</div>
-		<div class="mb-3">
-			<label for="imgGod" class="form-label">God's Image</label>
-			<input type="file" class="form-control" id="imgGod" aria-describedby="imagenLabel">
-		</div>
-		<button type="submit" class="btn btn-primary">Add</button>
-	</form>
-	`;
-}
-
-function removeGod(){
-	clearModal();
-	modalTitle.innerHTML = btnRemoveGod.textContent;
-	modalBody.innerHTML = 
-	`
-		<p>Deseas eliminar este Dios?</p>
-		<h4>Seguro?</h4>
-	`;
-}
-
 function editGod(){
 	clearModal();
 	modalTitle.innerHTML = btnEditGod.textContent;
@@ -194,11 +132,6 @@ function addGiant(){
 	`;
 }
 
-function removeGiant(){
-	clearModal();
-	modalTitle.innerHTML = btnRemoveGiant.textContent;
-}
-
 function editGiant(){
 	clearModal();
 	modalTitle.innerHTML = btnEditGiant.textContent;
@@ -230,3 +163,96 @@ function seeGiant(){
 	modalTitle.innerHTML = btnSeeGiant.textContent;
 }
 
+// Con JQuery y on nos permite acceder a cada boton dependiendo de los registros 
+const on = (element, event, selector, handler) => {
+	element.addEventListener(event, e => {
+		if(e.target.closest(selector)){
+			handler(e);
+		}
+	});
+}
+
+// FUNCION PARA AGREGAR DIOSES 
+on(document, "click", "#addGod", (e) => {
+	modalTitle.innerHTML = document.getElementById('addGod').textContent;
+	modalBody.innerHTML = `
+	<form id="formAddGod">
+		<div class="mb-3">
+			<label for="nameGod" class="form-label">Name in the Netflix Serie</label>
+			<input type="text" class="form-control" id="nameGod" aria-describedby="nameLabel">
+		</div>
+		<div class="mb-3">
+			<label for="nameGodRepresentation" class="form-label">God's name</label>
+			<input type="text" class="form-control" id="nameGodRepresentation" aria-describedby="nameLabel">
+		</div>
+		<div class="mb-3">
+			<label for="powerGod" class="form-label">God's powers</label>
+			<input type="text" class="form-control" id="powerGod" aria-describedby="powerLabel">
+		</div>
+		<div class="mb-3">
+			<label for="imgGod" class="form-label">God's Image</label>
+			<input type="file" class="form-control" id="imgGod" aria-describedby="imagenLabel">
+		</div>
+		<button type="submit" class="btn btn-primary" id="btnSubmit">Add</button>
+	</form>
+	`;
+	let formAddGod = document.getElementById('formAddGod');
+	formAddGod.addEventListener("submit", async function (e) {
+		e.preventDefault();
+		let nameGod = document.getElementById('nameGod').value;
+		let nameGodRepresentation = document.getElementById('nameGodRepresentation').value;
+		let powerGod = document.getElementById('powerGod').value;
+		let imgGod = document.getElementById('imgGod').files[0].name;
+		await fetch('http://localhost:3800/api/gods', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				Name:nameGod,
+				God:nameGodRepresentation,
+				Power:powerGod,
+				img:imgGod
+			})
+		})
+		.then(response => response.json())
+		.then(data=>{
+			obtenerDatos();
+			// modal.remove();
+		})
+	});
+});
+
+// FUNCION PARA BORRAR DIOSES 
+on(document, "click", "#removeGod", (e) => {
+  const fila = e.target.parentNode.parentNode.parentNode.parentNode;
+  const id = fila.firstElementChild.innerHTML;
+  alertify.confirm(
+    "Deseas eliminar este dios",
+    async function () {
+     await fetch('http://localhost:3800/api/god/'+id, {method:'DELETE'})
+	  .then(res => res.json())
+	  .then(() => location.reload())
+	  alertify.success("Eliminado")
+    },
+    function () {
+      alertify.error("Cancelar");
+    }
+  );
+});
+
+// FUNCION PARA BORRAR GIGANTES 
+on(document, "click", "#removeGiant", (e) => {
+	const fila = e.target.parentNode.parentNode.parentNode.parentNode;
+	const id = fila.firstElementChild.innerHTML;
+	alertify.confirm(
+	  "Deseas eliminar este gigante",
+	  function () {
+		fetch('http://localhost:3800/api/giant/'+id, {method:'DELETE'})
+		.then(res => res.json())
+		.then(() => location.reload())
+		alertify.success("Eliminado")
+	  },
+	  function () {
+		alertify.error("Cancelar");
+	  }
+	);
+  });
